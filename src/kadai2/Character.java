a90df7ae2cc8e7373bfdca244e1b079aad6c7a63
kojimahayato <241029@std.hi-joho.ac.jp>
2025-06-18 10:31:09 +0900

    kadai1

---
+++ src/kadai2/Character.java
@@ -0,0 +1,7 @@
+package kadai2;
+
+public abstract class Character {
+    String name;
+    public abstract void attack(Matango m);
+    public void run(){}
+}
---
+++ src/kadai2/Wizard.java
@@ -0,0 +1,13 @@
+package kadai2;
+
+public class Wizard extends Character {
+    public void attack(Matango m){
+        System.out.println(this.name + "の攻撃！");
+        System.out.println("魔法使いは火の玉を放った！");
+        System.out.println("敵に7のダメージを与えた");
+        m.hp-=7;
+    }
+    public void run(){
+        System.out.println(this.name + "は逃げ出した！");
+    }
+}
